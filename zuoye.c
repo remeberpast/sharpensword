16b664a11802eb3920423c137f64c5abccece5eb
remeberpast <90702638+remeberpast@users.noreply.github.com>
2021-10-05 10:34:21 +0800

    1

---
+++ zuoye.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("       *\n");
+	printf("      *\n");
+	printf("     *\n");
+	printf("*   *\n");
+	printf(" * *\n");
+	printf("  *\n");
+
+	return 0;
+
+}
+
