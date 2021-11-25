3e4ff07e451e78effecb61748a251755821c885d
remeberpast <90702638+remeberpast@users.noreply.github.com>
2021-11-25 23:10:45 +0800

    Add files via upload

---
+++ 11_24函数（交换变量）.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+////交换变量的函数
+//void swap(int* pa, int* pb)
+//{
+//	int tmp = *pa;
+//	*pa = *pb;
+//	*pb = tmp;
+//}
+//
+//int main(void)
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	printf("a=%d,b=%d\n", a, b);
+//	swap(&a, &b);
+//	printf("a=%d,b=%d\n", a, b);
+//	return 0;
+//}
