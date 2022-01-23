5dec0120297f97fa1744310b5aab8c650d3ebfaa
cw <90702638+remeberpast@users.noreply.github.com>
2022-01-23 11:04:05 +0800

    Add files via upload

---
+++ first.py
@@ -0,0 +1,52 @@
+print('hello world')
+print('why')
+'''
+三引号：多行注释
+'''
+#格式化：ctrl+alt+l(将代码规范)
+for i in range(10):
+    print(i)
+#
+名字 = 'zhangsan'
+print(名字)
+money = 20
+print(type(money))
+money = 20.9
+print(type(money))
+money = '一万'
+print(type(money))
+money = "一万"
+print(type(money))
+money = '''一万'''
+print(type(money))
+shi = '''
+        静夜诗
+         唐  李白
+  床前明月光，疑似地上霜。
+  举头望明月，低头思故乡。
+'''
+print(shi)
+
+# 布尔类型
+isLogin = True
+print(isLogin)
+isLogin =False
+print(isLogin)
+
+# userName = input('请输入：')
+# print(userName)
+# print(type(userName))
+# money = input('请输入：')
+# print(money)
+# print(type(money))
+
+# print(int(money)+1000)
+# print(money+str(1000))
+
+print(money,end='')
+
+
+
+
+
+
---
+++ 求一个三位数的各位数.py
@@ -0,0 +1,2 @@
+num = int(input('请输入三位整数：'))
+print(num % 10,num//10 % 10, num//100 % 10)
