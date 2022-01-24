8f799b3b235206dd46d5f00ec29dfeb0181ec4fc
cw <90702638+remeberpast@users.noreply.github.com>
2022-01-24 09:43:41 +0800

    Add files via upload

---
+++ 1_23.py
@@ -0,0 +1,42 @@
+# '''
+# 判断分数
+# '''
+# score = float(input('请输入分数：'))
+# print(80 <= score <= 100)
+# a=2
+# b=1
+# print(a or b)
+# print(b or a)
+
+# python格式化输出
+# age = 18
+# name = 'cw'
+# print('我今年%d岁，叫做%s' % (age, name))
+
+# a = 0b10111
+# print(int(a))
+# 八进制不能直接转换为十进制
+# b=0xa5
+# print(oct(b))
+# print(int(b))
+# c=oct(b)
+# print(c)
+# d=int(c)
+# print(d)
+
+# print('*-'*20)
+# #表示打印20次
+
+# print(~7)
+# print(bin(-9))
+# print(bin(~-4))
+# print(int(0b11111101))
+
+# n = 12
+# print(n>>1)
+# print(n>>2)
+# print(n>>3)
+# print(n>>4)
+
+
+
