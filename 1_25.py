7b8b4928f31d6e6e57a65f6d2ecc6b4c68293a59
cw <90702638+remeberpast@users.noreply.github.com>
2022-01-25 22:43:35 +0800

    Add files via upload

---
+++ 1_25.py
@@ -0,0 +1,22 @@
+# 可以使用下划线将数字分组
+# universe_age = 14_000_000_100
+# print(universe_age)
+
+# # 同时给多个变量赋值
+# x, y, z = 0, 1, 2
+# print(x, y, z)
+
+# 常量定义将所有字母大写
+# NUM = 100
+
+# 数字8
+# print(3+5)
+# print(9-1)
+# print(2**3)
+# print(int(64/8))
+# print(2*4)
+
+# 最喜欢的数
+# favorite_num = 7
+# message = f"my favorite number is {favorite_num}!"
+# print(message)
