ca9eb7db729aa0453a8145b9cd2c79308fc2434d
cw <90702638+remeberpast@users.noreply.github.com>
2022-01-28 09:55:42 +0800

    Add files via upload

---
+++ 1_27.py
@@ -0,0 +1,82 @@
+# for i in range(1, 21):
+#     print(i)
+
+# num = [i for i in range(1, 1000001)]
+# # for a in num:
+# #     print(a)
+# print(min(num))
+# print(max(num))
+# print(sum(num))
+
+# even_nums = [i for i in range(1, 22, 2)]
+# for i in even_nums:
+#     print(i)
+
+# three_nums = [i for i in range(3, 31, 3)]
+# for i in three_nums:
+#     print(i)
+
+# cubicNumbers = [i**3 for i in range(1, 21)]
+# for i in cubicNumbers:
+#     print(i)
+
+# cubicNumbers = []
+# for i in range(1, 21):
+#     cubicNumbers.append(i**3)
+# print(cubicNumbers)
+
+# numbers = [i for i in range(1, 1000001)]
+# for i in numbers[1:-1:3]:
+#     print(i)
+
+# numbers = [i for i in range(1, 1000001)]
+# print("The first three items in the list are:")
+# for i in numbers[0:3]:
+#     print(i)
+# print("Three items from the middle of the list are:")
+# for i in numbers[4998:5001]:
+#     print(i)
+# print("The last three items in the list are:")
+# for i in numbers[-3:]:
+#     print(i)
+
+# 自助餐
+# buffet = ('cupcake', 'watermelon', 'BBQ', 'fish', 'pork')
+# for i in buffet:
+#     print(i)
+# buffet = ('cupcake', 'orange', 'beaf', 'fish', 'chicken')
+# for i in buffet:
+#     print(i)
+# buffet[0] = "monkey"
+
+# numbers = [i for i in range(1, 1000, 11)]
+# a = int(input('请输入一个数'))
+# if a in numbers:
+#     print(1)
+# if a not in numbers:
+#     print(0)
+
+# 杀死那个外星人
+# colors = ['green', 'yellow', 'red']
+# alien_color = input('击杀了什么颜色的外星人：')
+# if alien_color == 'green':
+#     print('获得5分')
+# elif alien_color == 'yellow':
+#     print('获得10分')
+# else:
+#     print('获得15分')
+
+# 人生
+# age = int(input('请输入年龄：'))
+# if age < 2:
+#     print('婴儿')
+# elif age < 4:
+#     print('幼儿')
+# elif age < 13:
+#     print('儿童')
+# elif age < 20:
+#     print('青少年')
+# elif age < 65:
+#     print('成年人')
+# else:
+#     print('老年人')
