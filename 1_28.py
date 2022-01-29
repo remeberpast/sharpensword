2edd80d0e54a308dde2ad40faee146f3bbe9a592
cw <90702638+remeberpast@users.noreply.github.com>
2022-01-29 10:41:26 +0800

    Add files via upload

---
+++ 1_28.py
@@ -0,0 +1,53 @@
+# favorite_fruits = ['watermelon', 'orange', 'grape']
+# if 'watermelon' in favorite_fruits:
+#     print('You really like watermelon!')
+# if 'orange' in favorite_fruits:
+#     print('You really like orange!')
+# if 'grape' in favorite_fruits:
+#     print('You really like grape!')
+
+# users = []
+# if users:
+#     print('There are users.')
+# else:
+#     print('We need to find some users!')
+# users = ['zhang xun', 'guo yunzhi', 'tan long', 'hu lingkang', 'liu chun']
+# for user in users:
+#     if user == 'zhang xun':
+#         print(f'Hello {user}, would you like to see a status report?')
+#     else:
+#         print(f'Hello {user}, thank you for logging in again.')
+
+# 检查用户名
+# current_users = ['cw', 'end', 'Bob', 'CArRy', 'beauty']
+# new_users = ['cW', 'earth']
+# for new in new_users:
+#     i = 0
+#     for current in current_users:
+#         i = i+1
+#         if (new.lower()) == (current.lower()):
+#             print(f'{new}已被使用,请重新创建')
+#             break
+#     if i == len(current_users):
+#         print(f'{new}创建成功')
+
+# numbers = [str(i) for i in range(1, 10)]
+# for num in numbers:
+#     if num == '1':
+#         print(f'{num}st')
+#     elif num == '2':
+#         print(f'{num}nd')
+#     else:
+#         print(f'{num}th')
+
+# person = {'first_name': 'chun',
+#           'last_name': 'liu',
+#           'age': 18,
+#           'city': 'si chuan',
+#           }
+# print(person.get('first_name'))
+# print(person['last_name'])
+# print(person.get('age'))
+# print(person['city'])
+
+# 喜欢的数
