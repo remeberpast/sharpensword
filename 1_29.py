1bca21f439245eae7b318e82f9c3c10355bac7a3
cw <90702638+remeberpast@users.noreply.github.com>
2022-01-30 11:06:49 +0800

    Add files via upload

---
+++ 1_29.py
@@ -0,0 +1,13 @@
+# favorite_numbers = {
+#     'zhang xun': 7,
+#     'liu chun': 5,
+#     'wang yihao': 8,
+#     'fu yang': 4,
+#     'go go': 13
+#                     }
+# print(favorite_numbers['zhang xun'])
+# print(favorite_numbers.get('liu chun', 0))
+# print(favorite_numbers['wang yihao'])
+# print(favorite_numbers.get('fu yang', 0))
+# print(favorite_numbers['go go'])
+
