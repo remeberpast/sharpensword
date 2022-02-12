c0b4f063c65f8e62cbded820614c2fe2890701ef
cw <90702638+remeberpast@users.noreply.github.com>
2022-02-12 10:25:49 +0800

    Add files via upload

---
+++ 2_11.py
@@ -0,0 +1,21 @@
+# message = input('What kind of car do you want to rent：')
+# print(f'Let me see if I can find you a {message}.')
+
+# food = ""
+# while True:
+#     food = input('请输入配料：')
+#     if food == 'quit':
+#         break
+#     print(food)
+
+# age = 0
+# while True:
+#     age = int(input('How old are you?'))
+#     if age < 3:
+#         print('Congratulation! You are free!')
+#     elif age < 12:
+#         print('Please give me 10 dollars.')
+#     else:
+#         print('Please give me 15 dollars.')
+
+
