ebf7612db1f8bf49effac4dfb7999336d36cde5c
cw <90702638+remeberpast@users.noreply.github.com>
2022-02-19 19:40:08 +0800

    Add files via upload

---
+++ 2_19.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+int main()
+{
+	/*cout << "hello world" << endl;*/
+	cout << "Enter two numbers:" << endl;
+	int a = 0, b = 0;
+	cin >> a >> b;
+	cout << "The sum of " << a << " and " << b << " is " << a + b << endl;
+	system("pause");
+	return 0;
+}
