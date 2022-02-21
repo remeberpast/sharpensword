0af4cd07e41dc31960c99d13fe76b44bfa9c53bd
cw <90702638+remeberpast@users.noreply.github.com>
2022-02-21 22:14:18 +0800

    Add files via upload

---
+++ 2_20.cpp
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	/*int a = 0, b = 0;
+//	cin >> a >> b;
+//	cout << "a、b的积：" << a << " * " << b << " = " << a * b << endl;*/
+//
+////while循环与for循环
+//	/*int sum1 = 0;
+//	while (a <= 10)
+//	{
+//		sum1 += a;
+//		a++;
+//	}
+//	cout << "sum1 = " << sum1 << endl;
+//	int sum2 = 0;
+//	for (b = 0;b <= 10;b++)
+//	{
+//		sum2 += b;
+//	}
+//	cout << "sum2 = " << sum2 << endl;*/
+//
+////利用循环获得数量不定的数据输入
+//	/*int sum = 0;
+//	int value = 0;
+//	while (cin >> value)
+//	{
+//		sum += value;
+//	}
+//	cout << "sum = " << sum << endl;*/
+//
+////if语句
+//	////currVal是正在找的数，val是每次输入判断的数
+//	//int currVal = 0, val = 0;
+//	////读取数并判断连续出现次数
+//	//if (cin >> currVal)
+//	//{
+//	//	int count = 1;
+//	//	while (cin >> val)
+//	//	{
+//	//		if (val == currVal)
+//	//		{
+//	//			count++;
+//	//		}
+//	//		else
+//	//		{
+//	//			//不同的只出现
+//	//			cout << currVal << "出现了" << count << "次" << endl;
+//	//			//记录新的值，并充值计数器
+//	//			currVal = val;
+//	//			count = 1;
+//	//		}
+//	//	}
+//	//}
+//	system("pause");
+//	return 0;
+//}
+
+
+//using namespace std;
+//int main()
+//{
+//	int x = 10;
+//	while (x > 0)
+//	{
+//		cout << "nihao\n";
+//		x--;
+//	}
+//	return 0;
+//}
