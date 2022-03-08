61b5e6e81996bb5845b580f9ef787a6328cc00e3
cw <90702638+remeberpast@users.noreply.github.com>
2022-03-08 13:45:44 +0800

    Add files via upload

--- 3_1.cpp
+++ 3_1.cpp
@@ -1,20 +1,21 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <iostream>
-
-#include "sclass2_2_cdemo.h"
-#include "sclass2_3_date.h"
-using namespace std;
-
-int main()
-{
-	CDemo d;
-	d.SetI(20);
-	d.SetJ(10);
-	d.GetI();
-	d.GetJ();
-	
-	Date newDate;
-	newDate.Print();
-	system("pause");
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <iostream>
+//
+//#include "sclass2_2_cdemo.h"
+//#include "sclass2_3_date.h"
+//using namespace std;
+//
+//int main()
+//{
+//	/*CDemo d;
+//	d.SetI(20);
+//	d.SetJ(10);
+//	d.GetI();
+//	d.GetJ();
+//	
+//	Date newDate;
+//	newDate.Print();*/
+//
+//	system("pause");
+//	return 0;
+//}
