57b43ad5230e10f5cde2ffadbc2791642638ef8b
cw <90702638+remeberpast@users.noreply.github.com>
2022-03-20 19:39:17 +0800

    Add files via upload

--- python_repos.py
+++ python_repos.py
@@ -3,12 +3,35 @@ from requests.packages import urllib3
 
 urllib3.disable_warnings()
 # 窒息感API调用并处理响应
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+url = 'https://api.github.com/search/repositories?q=language:css&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers, verify=False)
 print(f"Status code: {r.status_code}")
 # 将api响应赋给一个变量
 response_dict = r.json()
+# 打印于total_count相关的值
+print(f"Total repositories: {response_dict['total_count']}")
 
+# 探索有关仓库的信息
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# 研究第一个仓库
+repo_dict = repo_dicts[0]
+
+
+print("\nSelected information about first repository")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
+    print('\n')
+# print(f"\nKeys: {len(repo_dict)}")
+# for key in sorted(repo_dict.keys()):
+#     print(key)
 # 处理结果
-print(response_dict.keys())
+# print(response_dict.keys())
